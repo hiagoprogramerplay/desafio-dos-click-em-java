419b8825dff43cd066d708e832fc2cf793bf7677
Hiagoprogramer_play <161398837+hiagoprogramerplay@users.noreply.github.com>
2024-03-11 16:15:18 -0300

    adição do projeto js, index e css

---
+++ app.js
@@ -0,0 +1,15 @@
+let titulo = document.querySelector ('h1');
+titulo.innerHTML = 'HORA DO DESAFIO';
+
+function exibirMensagemNoConsole1 () {
+    alert('o botão console foi clicado!')
+};
+
+function exibirMensagemNoConsole2() {
+    alert('eu amo JS')
+};
+
+function exibirMensagemNoConsole3 () {
+    cidade = prompt('Digite o nome de alguma cidade do Brasil!!!');
+    alert(`Estive em ${cidade} e lembrei de você!`)
+};
